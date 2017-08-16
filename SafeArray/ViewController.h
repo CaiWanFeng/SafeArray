9e9858d6ca35f3682bc3db6271e2da76dd572828
caiqiang <caiqiang@kuaijiankang.com>
2017-08-16 15:14:03 +0800

    Initial Commit

---
+++ SafeArray/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeArray
+//
+//  Created by 蔡强 on 2017/8/16.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SafeArray/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SafeArray
+//
+//  Created by 蔡强 on 2017/8/16.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
